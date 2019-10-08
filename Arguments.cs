0f82ba1a0011a1f2c9b7ed9f7198f4b08d8b1f6b
Senex626 <feelshadow5@gmail.com>
2019-10-08 16:55:27 +0300

    Initial file upload

---
+++ Arguments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Arguments
+{
+    private static readonly string APP_NAME = "Arguments";
+    static void Main(string[] args)
+    {
+        if(args == null || args.Length == 0)
+        {
+            MessageBox.Show("No arguments", APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        } else
+        {
+            string msg = "";
+            for (int i = 0; i < args.Length; ++i)
+            {
+                msg += (i + 1) + ". " + args[i] + Environment.NewLine;
+            }
+            MessageBox.Show(msg, APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
